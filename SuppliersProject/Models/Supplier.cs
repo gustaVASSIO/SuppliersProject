7f9a88b05e7c4a62aaa085c2265f27d0d271fa59
gustaVASSIO <guguzim.anacleto@gmail.com>
2023-02-15 16:32:55 -0300

    Adicionando alterações nas view e validações nos componentes.

--- SuppliersProject/Models/Supplier.cs
+++ SuppliersProject/Models/Supplier.cs
@@ -6,12 +6,21 @@ namespace SuppliersProject.Models
     [Table("suppliers")]
     public class Supplier
     {
+        [Required]
         [Display(Name="Id")]
         public int Id { get; set; }
+        
+        [Required]
+        [StringLength(100, MinimumLength =5,ErrorMessage ="The name length must be between 5 and 100")]
         [Display(Name="Name")]
         public string Name { get; set; }
+
+        [Required]
+        [StringLength(14,MinimumLength =14, ErrorMessage = "The CNPJ must have 14 digits")]
         [Display(Name="CNPJ")]
         public string Cnpj { get; set; }
+        
+        [Required]
         [Display(Name="Role")]
         public string Role { get; set; }
 
